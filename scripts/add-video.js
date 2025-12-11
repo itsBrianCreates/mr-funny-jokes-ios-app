e9341b35cd8c4650f588b10024b13c93d0015e47
itsBrianCreates <youremail@domain.com>
2025-12-10 20:24:36 -0800

    Add character images, app icon, and video scripts

---
+++ scripts/add-video.js
@@ -0,0 +1,9 @@
+{
+  localPath: "./videos/mr-funny-intro.mp4",
+  character: "mr_funny",  // or mr_potty, mr_bad, mr_love, mr_sad
+  title: "Meet Mr. Funny!",
+  description: "Optional description",
+  tags: ["wordplay"],
+  duration: 45  // seconds (optional)
+}
+
